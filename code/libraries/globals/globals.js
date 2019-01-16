04a6b1cf2ece16a575cb0b253baafa8a0b6871d8
AxeltaIngram <46739162+AxeltaIngram@users.noreply.github.com>
2019-01-16 11:58:23 +0530

    Asset created by AxeltaIngram

---
+++ code/libraries/globals/globals.js
@@ -0,0 +1,7 @@
+var auth_token="Y2xlYXJibGFkZTp3M0xjb20zQ0I=";	
+var ax_auth="QXhlbHRhOkF4ZWx0QTIh";
+var superUser_auth="aW5ncmFtLmxhYjp3M0xjb20zSW5ncmFt";
+//aW5ncmFtbWljcm8ucG9jOkZhbGxAMjAxOA==
+var fromAddr="pushpa@axelta.com"
+var mail_pwd="Ammananna@196";
+var platformURI = "https://ingrammicro.clearblade.com";
