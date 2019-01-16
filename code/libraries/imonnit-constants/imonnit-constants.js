648cea9e52aeb045c60046d563dc5df2cd998280
AxeltaIngram <46739162+AxeltaIngram@users.noreply.github.com>
2019-01-16 11:58:24 +0530

    Asset created by AxeltaIngram

---
+++ code/libraries/imonnit-constants/imonnit-constants.js
@@ -0,0 +1,5 @@
+var imonnit_uri = "https://www.imonnit.com";
+var imonnit_uri2 = "http://40.86.214.108";
+var update_url = "/json/GatewayUpdateFirmware/";
+var remove_url = "/json/RemoveGateway/";
+var reform_url = "/json/GatewayReform/";
