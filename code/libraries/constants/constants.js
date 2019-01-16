e14f0936083b6b2958b96c07a9c5a60884e3b4ce
AxeltaIngram <46739162+AxeltaIngram@users.noreply.github.com>
2019-01-16 11:58:21 +0530

    Asset created by AxeltaIngram

---
+++ code/libraries/constants/constants.js
@@ -0,0 +1,10 @@
+var customer_table = "Customer";
+var devices_metadata = "devices_metadata";
+var device_data = "device_data";
+var gateways_data = "gateways_data";
+var Networks = "Networks";
+var Sensors = "Sensors";
+var sensors_data = "sensors_data";
+var sys_values = "sys_values";
+var Testing = "Testing";
+var unrevealedDevicesData="unrevealed_devices_data";
